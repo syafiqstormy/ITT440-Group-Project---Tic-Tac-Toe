ef229095aa3bd477402030c275d03a562fee0d82
syafiqstormy <syafiqstormy@gmail.com>
2021-01-09 07:24:00 +0000

    added code

--- CliTac.py
+++ CliTac.py
@@ -20,17 +20,32 @@ def client_program():
 	#Receive input to send to server whether to play or not.
 	while True:
 		Input = input('Play? Enter Y or N :')
-		if Input == '':
-			print("Invalid input!")
-			Input = input('Play?Enter Y or N :')
+		#if Input == '':
+		#	print("Invalid input!")
+		#	Input = input('Play?Enter Y or N :')
 	#Converts input to uppercase and removes whitespaces
-	while Input.upper().strip() != 'N':
+	Input = Input.upper().strip()
+	while Input != 'N':
 		ClientSocket.send(Input.encode('utf-8'))
-		data = ClientSocket.recv(1024).decode('utf-8')
-
-		Response = ClientSocket.recv(1024)
+		While True:
+		Response = ClientSocket.recv(1024).decode()
+		#Receive input Do you want to be X or O?
 		if len(Response)!=0:
-			print(Response.decode('utf-8')
+			print(Response.decode())
+		playeropt = input("->")
+		playeropt = playeropt.upper()
+		ClientSocket.send(playeropt.encode())
+		Response= ClientSocket.recv(1024).decode()
+		while "Win" or "Tie" not in Response:
+			print("\nBoard:\n+ Response)
+			while True:
+				prompt = input("Enter index [1-9]")
+				if message in [str(i+1) for in range(9)]:
+					break
+				print("Please enter index [1-9]")
+			ClientSocket.send(prompt.encode())
+			data = s.recv(1024).decode()
+			print(data)
 
 
 
