f6e1fcd1c438ef5fd6e4a52565fa81b13a8f4ccb
syafiqstormy <syafiqstormy@gmail.com>
2021-01-09 12:24:26 +0000

    Fixed indentation errors but board doesnt return

--- CliTac.py
+++ CliTac.py
@@ -1,59 +1,60 @@
 import socket
 import os
-
+import sys
+import re
 
 # Client program
 def client_program():
-	host = '192.168.0.66'
-	port = 8080
-
-
-	os.system('clear')
-	ClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	
-	try:
-		ClientSocket.connect((host,port))
-	except socket.error as e:
-		print(str(e))
-		sys.exit()
-
-	#Receive input to send to server whether to play or not.
-	while True:
-		Input = input('Play? Enter Y or N :')
-		#if Input == '':
-		#	print("Invalid input!")
-		#	Input = input('Play?Enter Y or N :')
-	#Converts input to uppercase and removes whitespaces
-	Input = Input.upper().strip()
-	while Input != 'N':
-		ClientSocket.send(Input.encode('utf-8'))
-		While True:
-		Response = ClientSocket.recv(1024).decode()
-		#Receive input Do you want to be X or O?
-		if len(Response)!=0:
-			print(Response.decode())
-		x = re.search("Do",(str(Response)))
-		#if message contains option X or O do
-		if x:
-			playeropt = input("->")
-			playeropt = playeropt.upper()
-			ClientSocket.send(playeropt.encode())
-			Response= ClientSocket.recv(1024).decode()
-		while "Win" or "Tie" not in Response:
-			print("\nBoard:\n+ Response)
-			while True:
-				prompt = input("Enter index [1-9]")
-				if message in [str(i+1) for in range(9)]:
-					break
-				print("Please enter index [1-9]")
-			ClientSocket.send(prompt.encode())
-			data = s.recv(1024).decode()
-			print(data)
-
-
-
+    host = '192.168.0.66'
+    port = 8888
+    os.system('clear')
+    ClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        ClientSocket.connect((host, port))
+    except socket.error as e:
+        print(str(e))
+        sys.exit()
+    # Receive input to send to server whether to play or not.
+    while True:
+        #initOpt = input('Play? Enter Y or N :')
+        # // if Input == '': // print("Invalid input!") Input =
+        # input('Play?Enter Y or N :') Converts input to uppercase and
+        # removes whitespaces
+        #initOpt = initOpt.upper().strip()
+        initOpt = ClientSocket.recv(1024).decode()
+        print(initOpt)
+        cliOpt = input('>')
+        while cliOpt != 'N':
+            ClientSocket.send(cliOpt.encode('utf-8'))
+            Response = ClientSocket.recv(1024).decode()
+            while True:
+            # If response exists
+                if len(Response) != 0:
+                    print(Response)
+                    x = re.search("Do", (str(Response)))
+                    # if message contains option X or O do
+                    if x:
+                        playerOpt = input("->")
+                        playerOpt = playerOpt.upper()
+                        ClientSocket.send(playerOpt.encode())
+                        Response = ClientSocket.recv(1024).decode()
+                    while "Win" or "Tie" not in Response:
+                        print("\nBoard:\n"+ Response)
+                        while True:
+                            index = input("Enter index [1-9]")
+                            if Response in [str(i+1) for i in range(9)]:
+                                break
+                            print("vreak code")
+                        ClientSocket.send(index.encode())
+                        data = ClientSocket.recv(1024).decode()
+                    print(data)
+                #if response doesnt exit
+                else:
+                    print("****Server has unexpectedly closed connection****")
+                    ClientSocket.close()
+                    sys.exit(0)
+        print("Thanks for entering. Goodbye")
+        sys.exit()
 if __name__ == '__main__':
-	client_program()
+    client_program()
 
-
-	ClientSocket.close()
--- ServTac.py
+++ ServTac.py
@@ -1,76 +1,81 @@
-#server side 
+# server side
 
+import re
 import socket
 import sys
 from _thread import *
 
-#init global variables here
+# init global variables here
 # --
-board = [' ' for x in range(10)]
+board = [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]]
 player_list = []
 count = 0
-num2Eng = {0:' ',1:'0',4:'X'}
-available = [(i,j) for i in range(3) for j in range(3)] 
-pointerEL = {(i*3)+j+1:(i,j) for i in range(3) for j in range(3)}
+num2Eng = {0: ' ', 1: '0', 4: 'X'}
+available = [(i, j) for i in range(3) for j in range(3)]
+pointerEL = {(i * 3) + j + 1: (i, j) for i in range(3) for j in range(3)}
+ThreadCount = 0
 
 
-
-#Server Initialization
+# Server Initialization
 def server_start():
-	host = ''
-	port = 8888
+    global ThreadCount
+    ThreadCount=0
+    host = ''
+    port = 8888
 
-	# Create TCP socket
-	ServerSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    # Create TCP socket
+    ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+    # Binding
+    try:
+        ServerSocket.bind((host, port))
+    except socket.error as e:
+        print("Bind error")
+        print(str(e))
+        sys.exit()
+    # Listen for connections
+    ServerSocket.listen(5)
+    print("Listening for connections...")
 
-	#Binding
-	try: 
-		ServerSocket.bind((host, port))
-	except socket.error as e:
-		print(str(e))
-		sys.exit()
-	#Listen for connections
-	ServerSocket.listen(5)
-	print("Listening for connections...")
-
-	#Thread start
-	while True:
-		conn, address = ServerSocket.accept()
-		print('Connected to:' + address[0] +':'+str(address[1]))
-		start_new_thread(threaded_client, (conn, address))
-		ThreadCount += 1
-		print('Thread Number:' + str(ThreadCount))
-	ServerSocket.close()
+    # Thread start
+    while True:
+        conn, address = ServerSocket.accept()
+        print('Connected to:' + address[0] + ':' + str(address[1]))
+        start_new_thread(threaded_client, (conn, address))
+        ThreadCount += 1
+        print('Thread Number:' + str(ThreadCount))
+    ServerSocket.close()
 
 
-# send grid with number to user
 
-#receive input from clients yes or no to play
+# receive input from clients yes or no to play
 def threaded_client(conn, addr):
     welcome = "**Welcome to the Tic Tac Toe Server! :D**\nDo you want to play the game?(yes,no) No to quit"
-    player_list.append(conn)
-    count = count + 1
+    #player_list.append(conn)
+    #count = count + 1
     try:
-    	#send welcome message to client
-        conn.send(welcome.encode(encoding='UTF-8',errors='strict'))
+        # send welcome message to client
+        conn.send(welcome.encode(encoding='UTF-8', errors='strict'))
         while True:
-        	#receive input from client
-            data = conn.recv(2048)
-            #break if no input/error
+            # receive input from client
+            data = conn.recv(2048).decode()
+            # break if no input/error
             if not data:
                 break
-            #verify input
+            # verify input
             if (data == "Y"):
-            	initGame(conn, addr)
-            #client quit
-            else:
-		remove player_list latest data
-		del player_list[-1]
-		count=count -1
-		goodbye_msg = "Goodbye"
-		print("Client rejected game")
-		conn.send(goodbye_msg.encode(encoding='utf-8', errors='strict'))
-            	return
+                initGame(conn, addr)
+            # client quit
+            elif (data =="N"):
+                # remove player_list latest data
+                del player_list[-1]
+                #count = count - 1
+                goodbye_msg = "Goodbye"
+                print("Client rejected game")
+                conn.send(goodbye_msg.encode(encoding='utf-8', errors='strict'))
+                return
 
     except socket.error as e:
         print("Socket error: %s" % str(e))
@@ -78,9 +83,7 @@ def threaded_client(conn, addr):
     conn.close()
 
 
-
-
-#generate board
+# generate board
 def generateBoard():
     b = ''
     for i in range(5):
@@ -94,8 +97,10 @@ def generateBoard():
         b += '\n'
     return b
 
-#player moves
-def playerMove(conn,player,data)
+
+
+# player moves
+def playerMove(conn, player, data):
     i = int(data[0])
     j = int(data[1])
 
@@ -107,12 +112,11 @@ def playerMove(conn,player,data)
     available.pop(available.index((i, j)))
 
 
-
-#check if win
-def checkWin()
-    #checks if the board is full or not 
+# check if win
+def checkWin():
+    # checks if the board is full or not
     if len(available) == 0:
-    #returns draw if board is full
+        # returns draw if board is full
         return "Draw"
     column = [0, 0, 0]
     diagonal = [0, 0]
@@ -130,7 +134,7 @@ def checkWin()
     for i in range(3):
         if column[i] == 3:
             return 'O Win!'
-        elif coloumn[i] == 12:
+        elif column[i] == 12:
             return 'X Win!'
     for i in range(2):
         if diagonal[i] == 3:
@@ -139,47 +143,52 @@ def checkWin()
             return 'X Win!'
     return 'Noone'
 
-
-#receive input from player whether X or O
+# receive input from player whether X or O
 def initGame(conn, addr):
-	#first player 
-	if count == 1
-		msgXO = generateBoard() + "\n\nDo you want to be O or X? [O/X]: "
-		conn.send(msgXO.encode())
-		data = conn.recv(1024).decode()
-		print(addr[0] +" is "+ str(data))
-	if count == 2
-		if data == 'X'
-			msgXO = generateBoard() +"\n\nYou are O!"
-		else
-			msgXO = generateBoard() +"\n\nYou are X!"
+    # first player
+    data = ""
+    print(ThreadCount)
+    if ThreadCount == 1:
+        msgXO = generateBoard() + "\n\nDo you want to be O or X? [O/X]: "
+        conn.send(msgXO.encode())
+        data = conn.recv(1024).decode()
+        print(addr[0] + " is " + str(data))
+    if ThreadCount == 2:
+        if data == 'X':
+            msgXO = generateBoard() + "\n\nYou are O!"
+            conn.send(msgXO.encode())
+            print(addr[0] + " is O")
+        else:
+            msgXO = generateBoard() + "\n\nYou are X!"
+            conn.send(msgXO.encode())
+            print(addr[0] + " is X")
+
+    if data == 'X':
+        player1 = 4
+        player2 = 1
+    else:
+        player1 = 1
+        player2 = 4
+
+    data=""
+    # While no one has won yet
+    while checkWin() == 'Noone':
+        if(data != "quit"):
+            # sends updated board
+            update = generateBoard()
+            conn.send(update.encode())
+            # receive input from either client
+        data = conn.recv(1024).decode()
+        # convert the player move to int to occupy the space in pointer
+        data = pointerEL[int(data)]
+        playerMove(conn, player1, data)
+        if checkWin() != "Noone":
+            sys.exit()
 
 
-	if data == 'X'
-		player1 = 4
-		player2 = 1
-	else
-		player1 = 1
-		player2 = 4
-
-
-#While no one has won yet
-while checkWin() == 'Noone':
-	#sends updated board
-	update = generateBoard() + "\nEnter number: " 
-	conn.send(update.encode())
-	#receive input from client
-	data = conn.recv(1024).decode()
-
-	#convert the player move to int to occupy the space in pointer
-	data = pointerEL[int(data)]
-	playerMove(conn,player1,data)
-	if checkWin()!="Noone"
-		break
-
-message = generateBoard() + '\n\n' + checkWin()
-conn.send(message.encode()) 
-
+    message = generateBoard() + '\n\n' + checkWin()
+    conn.send(message.encode())
 
 if __name__ == "__main__":
-	server_start()
+    server_start()
+
